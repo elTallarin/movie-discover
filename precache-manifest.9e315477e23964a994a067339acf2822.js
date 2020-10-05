ae980d9e3295096874db398c956aead223544f89
Matias Strizzolo <matiasstrizzolo@gmail.com>
2020-10-05 01:41:26 -0300

    Updates

---
+++ precache-manifest.9e315477e23964a994a067339acf2822.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e1d39f32dcef3ba8c8a0f57388c57b",
+    "url": "/movie-discover/index.html"
+  },
+  {
+    "revision": "b8eb6950448e267b2ccb",
+    "url": "/movie-discover/static/css/2.30d76334.chunk.css"
+  },
+  {
+    "revision": "11df0e6d11ec6ef74891",
+    "url": "/movie-discover/static/css/main.8f936fd8.chunk.css"
+  },
+  {
+    "revision": "b8eb6950448e267b2ccb",
+    "url": "/movie-discover/static/js/2.d8cd0c6b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/movie-discover/static/js/2.d8cd0c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11df0e6d11ec6ef74891",
+    "url": "/movie-discover/static/js/main.d68b9285.chunk.js"
+  },
+  {
+    "revision": "da97170c6c3c3b6a1496",
+    "url": "/movie-discover/static/js/runtime-main.5bb85f8d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movie-discover/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movie-discover/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movie-discover/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movie-discover/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movie-discover/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
